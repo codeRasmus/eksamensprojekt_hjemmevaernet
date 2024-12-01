733c8615cee3330e1bf4f1a4426c80cda22466a1
Rasmus <rasmusmllr@gmail.com>
2024-12-01 20:18:56 +0100

    burger menu hath begunneth

---
+++ modules/burgerMenu.js
@@ -0,0 +1,8 @@
+export function toggleBurgerMenu() {
+  const burgerIcon = document.getElementById("burger_icon");
+  const burgerNav = document.getElementById("burger_nav");
+
+  burgerIcon.addEventListener("click", () => {
+    burgerNav.classList.toggle("active");
+  });
+}
--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 "use strict";
+
 // MODULE IMPORTS
 import { fontSizer } from "./modules/fontSizer.js";
 import { handleUserPrompt } from "./modules/promptHandler.js";
 import { storeDataLocal } from "./modules/storageModule.js";
 import { loadAni, stopLoadAni } from "./modules/loadAni.js";
+import { toggleBurgerMenu } from "./modules/burgerMenu.js";
 
 // GLOBALE VARIABLER & KONSTANTER
 const increaseButton = document.getElementById("increaseFont");
@@ -15,20 +17,21 @@ const scalingFactorDown = 0.9;
 increaseButton.addEventListener("click", () => fontSizer(scalingFactorUp));
 decreaseButton.addEventListener("click", () => fontSizer(scalingFactorDown));
 
-window.addEventListener("load", async function() {
-    loadAni();
-    try {
-        // fake fetching data
-        await fetchData();
-    } finally {
-        stopLoadAni();
-    }
+toggleBurgerMenu();
+
+window.addEventListener("load", async function () {
+  loadAni();
+  try {
+    // fake fetching data
+    await fetchData();
+  } finally {
+    stopLoadAni();
+  }
 });
 
 async function fetchData() {
-    // fake a fetch call
-    return new Promise((resolve) => setTimeout(resolve, 2000));
+  // fake a fetch call
+  return new Promise((resolve) => setTimeout(resolve, 2000));
 }
 
 handleUserPrompt("userprompt", "sendprompt_btn");
-
